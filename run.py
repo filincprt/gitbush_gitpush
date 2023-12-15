04815affd10eb1f86801ed3d514d6772c3c6adf4
filin_cprt <61471737+filincprt@users.noreply.github.com>
2023-12-15 09:49:29 +0500

    Первый

---
+++ run.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def lagrange_polynomial(x, nodes):
+    result = 0
+    for i in range(len(nodes)):
+        term = nodes[i][1]
+        for j in range(len(nodes)):
+            if i != j:
+                term *= (x - nodes[j][0]) / (nodes[i][0] - nodes[j][0])
+        result += term
+    return result
+
+# Узловые точки
+nodes = [(1, -3), (2, -2), (4, 2), (7, 8)]
+
+x_values = np.linspace(0, 8, 400)
+y_values = [lagrange_polynomial(x, nodes) for x in x_values]
+
+plt.plot(x_values, y_values, label='Interpolation Polynomial')
+plt.scatter(*zip(*nodes), color='red', label='Nodes')
+plt.xlabel('x')
+plt.ylabel('P(x)')
+plt.legend()
+plt.title('Interpolation Polynomial and Nodes')
+plt.grid(True)
+plt.show()
